34692bd40017671574fdb09957c5db79413f775b
Andreas Kühn <andreas@pit-experts.de>
2023-11-10 14:38:16 +0100

    End of subsection 51

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  model: any = {}
 
+  constructor() {} //Im Kurs stand die Zeile da
+
+  ngOnInit(): void {} //Im Kurs stand die Zeile da
+
+  login() {
+    console.log(this.model);
+  }
 }
