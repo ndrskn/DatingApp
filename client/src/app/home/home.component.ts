6dd38c8025e76261e0f73018752f63d107dd4e86
Andreas Kühn <andreas@pit-experts.de>
2023-11-13 10:40:24 +0100

    End of subsection 62

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -27,5 +27,9 @@ export class HomeComponent implements OnInit {
 			complete: () => console.log('Request has completed')
 		  })
 	}
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
+  }
  
 }
